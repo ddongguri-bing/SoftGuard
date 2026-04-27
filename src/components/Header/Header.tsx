b81cbed486185854805c07a6ca985e5d5f08be8d
ddongguri-bing <ckdalssnsk@naver.com>
2026-04-27 16:14:06 +0900

    🩹 FIX: 로고 이미지 비율 유지 경고 수정

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,14 @@ import LocationDropdown from "./LocationDropdown";
 export default function Header() {
   return (
     <div className="flex gap-25 py-7.5 pl-12.5">
-      <Image src={Logo} width={111} height={25} alt="Soft Guard 로고" />
+      <Image
+        src={Logo}
+        width={111}
+        height={25}
+        alt="Soft Guard 로고"
+        loading="eager"
+        className="h-auto w-auto"
+      />
       <div className="flex items-center gap-7.5">
         <LocationDropdown />
         <HeaderDate />
