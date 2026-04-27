87b24429841d881237c094b3b244735325031b8d
ddongguri-bing <ckdalssnsk@naver.com>
2026-04-27 15:59:00 +0900

    ♻️ REFACTOR: 시간 하이드레이션 문제로 로직 수정

--- src/components/Header/HeaderDate.tsx
+++ src/components/Header/HeaderDate.tsx
@@ -4,12 +4,15 @@ import { formatNow } from "@/utils/timeUtils";
 import { useEffect, useState } from "react";
 
 export default function HeaderDate() {
-  const [now, setNow] = useState(new Date());
+  const [now, setNow] = useState<Date | null>(null);
 
   useEffect(() => {
+    setNow(new Date());
     const id = setInterval(() => setNow(new Date()), 1000);
     return () => clearInterval(id);
   }, []);
 
-  return <div>{formatNow(now)}</div>;
+  return (
+    <div className="text-body-medium">{now ? formatNow(now) : "--:--:--"}</div>
+  );
 }
