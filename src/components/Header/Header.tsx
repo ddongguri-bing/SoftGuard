9f16e04142d336fa9691fd5bc328448eba696c16
ddongguri-bing <ckdalssnsk@naver.com>
2026-04-28 09:21:23 +0900

    💄 STYLE: 양옆 패딩을 최상단으로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ import LocationDropdown from "./LocationDropdown";
 
 export default function Header() {
   return (
-    <div className="flex gap-25 py-7.5 pl-12.5">
+    <div className="flex gap-25 py-7.5">
       <Image
         src={Logo}
         width={111}
