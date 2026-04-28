aa7b59854599870c4bb86eaf10140038a1ceae8b
ddongguri-bing <ckdalssnsk@naver.com>
2026-04-28 10:48:45 +0900

    💄 STYLE: 이벤트 스트림 영역 확보

--- src/components/EventStream/EventStream.tsx
+++ src/components/EventStream/EventStream.tsx
@@ -2,7 +2,7 @@ import EventStreamItems from "./EventStreamItems";
 
 export default function EventStream() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-1 flex-col">
       <div className="mb-2.5 flex items-baseline gap-2.5">
         <div className="text-body-medium-bold">Near-miss 이벤트 스트림</div>
         <div className="text-body-xsmall-bold text-white-third">실시간</div>
