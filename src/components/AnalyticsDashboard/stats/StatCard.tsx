f93fe9965fe6b9d2f9cd4ccff3ccc069accefb0f
ddongguri-bing <ckdalssnsk@naver.com>
2026-04-29 10:25:06 +0900

    ♻️ StatCard의 0값 렌더링 보장

--- src/components/AnalyticsDashboard/stats/StatCard.tsx
+++ src/components/AnalyticsDashboard/stats/StatCard.tsx
@@ -10,8 +10,8 @@ export default function StatCard({ title, count, percentage }: StatCardProps) {
   return (
     <div className="border-white-first bg-white-first/5 flex w-full flex-col items-center justify-between gap-2.5 rounded-[10px] border pt-4.5 pb-4">
       <div className="text-body-xsmall-bold text-white-third">{title}</div>
-      {count && <MetricValue value={count} unit="건" />}
-      {percentage && <MetricValue value={percentage} unit="%" />}
+      {count !== undefined && <MetricValue value={count} unit="건" />}
+      {percentage !== undefined && <MetricValue value={percentage} unit="%" />}
     </div>
   );
 }
