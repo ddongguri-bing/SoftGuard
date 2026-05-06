c695b3d108d2119eb6424256216e057321a75ab6
ddongguri-bing <ckdalssnsk@naver.com>
2026-05-06 15:38:16 +0900

    💄 Header 컴포넌트: 전체보기 페이지의 경우 뒤로가기 버튼 추가 및 라우팅 기능 구현

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,35 @@
-import Image from "next/image";
+"use client";
 
 import Logo from "@/assets/logo.svg";
+
+import BackArrow from "@/assets/arrow-back.svg";
+import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
 import HeaderDate from "./HeaderDate";
 import HeadWheather from "./HeadWheather";
 import LocationDropdown from "./LocationDropdown";
 
 export default function Header() {
+  const router = useRouter();
+
+  const pathname = usePathname();
+  const isEventStream = pathname.startsWith("/eventStream");
+
+  if (isEventStream) {
+    return (
+      <header className="flex py-7.5">
+        <button
+          type="button"
+          onClick={() => router.replace("/")}
+          aria-label="뒤로가기"
+          className="inline-flex items-center"
+        >
+          <Image src={BackArrow} width={30} height={30} alt="뒤로가기" />
+        </button>
+      </header>
+    );
+  }
+
   return (
     <header className="flex gap-25 py-7.5">
       <Image
