578dbdb24b2c6583ffc5c8308d75fd88b64bd3bc
ddongguri-bing <ckdalssnsk@naver.com>
2026-05-06 15:20:05 +0900

    💄 레이아웃 수정: Header 컴포넌트 추가 및 스타일 조정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import AnalyticsDashboard from "@/components/AnalyticsDashboard/AnalyticsDashboard";
 import DeviceStatus from "@/components/DeviceStatus/DeviceStatus";
 import EventStream from "@/components/EventStream/EventStream";
-import Header from "@/components/Header/Header";
 import InsightPanel from "@/components/InsightPanel/InsightPanel";
 import ScenarioSelector from "@/components/ScenarioSelector/ScenarioSelector";
 import SituationPanel from "@/components/SituationPanel/SituationPanel";
@@ -9,8 +8,7 @@ import VideoPanel from "@/components/VideoPanel/VideoPanel";
 
 export default function Home() {
   return (
-    <div className="mb-25 flex flex-col gap-12.5 px-12.5">
-      <Header />
+    <div className="mb-25 flex flex-col gap-12.5">
       <SituationPanel />
       <section className="flex gap-12.5">
         <VideoPanel />
