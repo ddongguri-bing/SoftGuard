6ee41921778b9ebc7e95f1eb7c0e23521d6f4ffa
ddongguri-bing <ckdalssnsk@naver.com>
2026-05-08 10:23:33 +0900

    ♻️ 대시보드: SectionHeader에 label 및 labelClassName 추가

--- src/components/AnalyticsDashboard/AnalyticsDashboard.tsx
+++ src/components/AnalyticsDashboard/AnalyticsDashboard.tsx
@@ -5,7 +5,11 @@ import StatsRow from "./stats/StatsRow";
 export default function AnalyticsDashboard() {
   return (
     <section className="flex flex-5 flex-col gap-2.5">
-      <SectionHeader title="통계 대시보드" />
+      <SectionHeader
+        title="통계 대시보드"
+        label="전주 기준"
+        labelClassName="text-white-third"
+      />
       <div className="flex flex-col gap-2.5">
         <ChartsRow />
         <StatsRow />
