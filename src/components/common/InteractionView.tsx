b20b143e5ade3f2c1faba73a4b052eaddbcce600
ddongguri-bing <ckdalssnsk@naver.com>
2026-05-13 16:08:49 +0900

    🔧 InteractionView 컴포넌트의 object1 및 object2 프로퍼티를 선택적으로 변경하고 코드 정리

--- src/components/common/InteractionView.tsx
+++ src/components/common/InteractionView.tsx
@@ -1,10 +1,9 @@
+import DoubleHeadedArrow from "@/assets/double-headed-arrow.svg";
 import Image from "next/image";
 
-import DoubleHeadedArrow from "@/assets/double-headed-arrow.svg";
-
 interface InteractionViewProps {
-  object1: string | undefined;
-  object2: string | undefined;
+  object1?: string | undefined;
+  object2?: string | undefined;
   size: "small" | "medium";
   danger?: boolean;
 }
@@ -15,19 +14,31 @@ export default function InteractionView({
   size,
   danger,
 }: InteractionViewProps) {
+  const collisionAccident = object1 !== null && object2 !== null;
   return (
-    <div
-      className={`flex gap-1.25 ${size === "small" ? "text-body-small-bold" : "text-body-medium-bold"}`}
-    >
-      <div>{object1}</div>
-      <Image
-        src={DoubleHeadedArrow}
-        width={24}
-        height={24}
-        alt="양방향 화살표"
-      />
-      <div>{object2}</div>
-      {danger && <div>충돌 위험</div>}
-    </div>
+    <>
+      {collisionAccident ? (
+        <div
+          className={`flex gap-1.25 ${size === "small" ? "text-body-small-bold" : "text-body-medium-bold"}`}
+        >
+          <div>{object1}</div>
+          <Image
+            src={DoubleHeadedArrow}
+            width={24}
+            height={24}
+            alt="양방향 화살표"
+          />
+          <div>{object2}</div>
+          {danger && <div>충돌 위험</div>}
+        </div>
+      ) : (
+        <div
+          className={`flex gap-1.25 ${size === "small" ? "text-body-small-bold" : "text-body-medium-bold"}`}
+        >
+          {object1}
+          {object2}
+        </div>
+      )}
+    </>
   );
 }
