c88213fa7508d65ba6547067140dde06a44caac0
ddongguri-bing <ckdalssnsk@naver.com>
2026-05-13 15:40:24 +0900

    💄 EventDetailText 컴포넌트에서 액션 텍스트 처리 로직 개선

--- src/components/EventStream/EventDetailText.tsx
+++ src/components/EventStream/EventDetailText.tsx
@@ -8,9 +8,13 @@ export default function EventDetailText({ data }: EventDetailTextProps) {
   const hasTrafficCounts =
     data.vehicle_count != null && data.pedestrian_count != null;
 
+  const hasAction = data.action != null;
+
   const detailText = hasTrafficCounts
     ? `${data.location} | 차량 ${data.vehicle_count}, 보행자 ${data.pedestrian_count} | ${data.action}`
-    : `${data.location} | ${data.action}`;
+    : hasAction
+      ? `${data.location} | ${data.action}`
+      : `${data.location}`;
 
   return <div className="text-body-small text-white-third">{detailText}</div>;
 }
